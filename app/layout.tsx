dce97d38946e87bdc646349d077ca92d80fb9294
Paulino Gonçalves Bambuca <paulinogoncalvesb@gmail.com>
2026-09-05 09:59:17 +0100

    Atualização do sistema escolar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Portal Hebrom III",
   description: "Portal de notícias Hebrom III",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
